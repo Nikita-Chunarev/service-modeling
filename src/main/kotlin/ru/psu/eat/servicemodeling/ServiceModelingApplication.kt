c7c65f630655a6e061789e8660e32688558d6807
Nikita <116355578+Nikita-Chunarev@users.noreply.github.com>
2023-02-24 01:00:17 +0500

    Пустой проект

---
+++ src/main/kotlin/ru/psu/eat/servicemodeling/ServiceModelingApplication.kt
@@ -0,0 +1,11 @@
+package ru.psu.eat.servicemodeling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceModelingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceModelingApplication>(*args)
+}
